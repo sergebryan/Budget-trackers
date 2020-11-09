31c36525b2095367a2de454f8684f971c5431886
Sergebryan <sergebryan1@gmail.com>
2020-11-09 00:11:54 -0600

    completed homework

--- Develop/server.js
+++ Develop/server.js
@@ -20,9 +20,9 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
+//app.get("/", (req, res) => {
+//  res.sendFile(path.join(__dirname, "public/index.html"));
+//});
 
 // routes
 app.use(require("./routes/api.js"));
