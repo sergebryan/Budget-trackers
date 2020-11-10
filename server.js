e881028798d998010429d700d305c642a92dd698
Sergebryan <sergebryan1@gmail.com>
2020-11-09 22:08:52 -0600

    completed homework

--- server.js
+++ server.js
@@ -15,14 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.dblink || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-//app.get("/", (req, res) => {
-//  res.sendFile(path.join(__dirname, "public/index.html"));
-//});
 
 // routes
 app.use(require("./routes/api.js"));
